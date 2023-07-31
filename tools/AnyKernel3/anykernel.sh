b27e0d4dfa1ff0f68b28f864723a94db09edfbdc
Coconut <coconutat@protonmail.com>
2023-07-31 15:58:06 +0800

    Add AnyKernel 3

---
+++ tools/AnyKernel3/anykernel.sh
@@ -0,0 +1,38 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Redmi Note 12 Pro Kernel -- Coconutat @ Github
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+
+## boot shell variables
+block=boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
