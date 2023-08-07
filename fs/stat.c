9fc47130976d64cbca62634cf57dc11de6e3bed5
Coconut <coconutat@protonmail.com>
2023-08-07 12:10:34 +0800

    Revert "Revert "KernelSU:Manually modify the kernel source For Kprobes won't work""
    This reverts commit 9c3423b068360233aa1578f15cdc750b149c583d.

--- fs/stat.c
+++ fs/stat.c
@@ -163,6 +163,10 @@ EXPORT_SYMBOL(vfs_statx_fd);
  *
  * 0 will be returned on success, and a -ve error code if unsuccessful.
  */
+
+/* KernelSU */
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+
 int vfs_statx(int dfd, const char __user *filename, int flags,
 	      struct kstat *stat, u32 request_mask)
 {
@@ -170,6 +174,8 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
 
+	ksu_handle_stat(&dfd, &filename, &flags);
+	
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
